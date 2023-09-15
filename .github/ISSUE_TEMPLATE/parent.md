bd56bacdd90e0837c79181cbb2df7b01955035d3
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2023-09-15 10:03:52 -0400

    Initial stuff

---
+++ .github/ISSUE_TEMPLATE/parent.md
@@ -0,0 +1,14 @@
+---
+name: Parent Issue
+about: A major initiative we'd like to tackle
+title: "[Parent]"
+labels: parent
+assignees: ''
+---
+
+
+```[tasklist]
+### Work Breakdown
+- [ ] TODO 1
+- [ ] TODO 2
+```
---
+++ env/dev/clj/user.clj
@@ -0,0 +1,30 @@
+(ns user
+  (:require [aero.core :refer [read-config]]
+            [clojure.tools.logging :as log]
+            [clojure.tools.namespace.repl :as repl]
+            [clojure.java.io]
+            [integrant.core :as ig]
+            [integrant.repl :refer [prep go halt reset]]
+            [integrant.repl.state]))
+
+
+(defmethod ig/init-key :system/env [_ env]
+  (log/debugf "Starting system using environment %s" env))
+
+(defmethod aero.core/reader 'ig/ref
+  [_ _ value]
+  (ig/ref value))
+
+(def config (read-config (clojure.java.io/resource "system.edn") {:profile :dev}))
+
+(integrant.repl/set-prep! #(ig/prep config))
+(repl/set-refresh-dirs "src/clj" "env/dev/clj")
+
+
+;; Helpers to start and stop the system
+(comment
+  (prep)
+  integrant.repl.state/config
+  (go)
+  (halt)
+  (reset))
