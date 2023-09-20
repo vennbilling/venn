3282a99a2f9771433c24911d42ca684427b7b125
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2023-09-20 14:05:58 -0400

    Improve logging when starting up. Add Banner (#10)

--- src/clj/venn/agent/core.clj
+++ src/clj/venn/agent/core.clj
@@ -10,9 +10,11 @@
 (defonce system (atom nil))
 
 (defn stop-app []
+  ((or (:stop defaults) (fn [])))
   (some-> (deref system) (ig/halt!)))
 
 (defn start-app []
+  ((or (:start defaults) (fn [])))
   (->> (config (:opts defaults))
        (ig/prep)
        (ig/init)
