e18f0c61be33a76dae4cb17a2da4f2c0b3867da9
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2023-09-27 14:08:40 -0400

    Use malli coercion properly. Add billing_details

--- src/clj/venn/agent/http/routes.clj
+++ src/clj/venn/agent/http/routes.clj
@@ -18,8 +18,10 @@
                                {:status 200 :body {:db (xt/status db)}})}}])
 
 (defn api-routes [_opts]
-  ["/identify" {:post {:parameters {:body {:identifier :string
-                                           :traits :map}}
+  ["/identify" {:post {:parameters {:body [:map
+                                           [:identifier string?]
+                                           [:traits map?]
+                                           [:billing_provider {:default {}} map?]]}
                        :responses {201 :map}
                        :handler customers/upsert!}}])
 
