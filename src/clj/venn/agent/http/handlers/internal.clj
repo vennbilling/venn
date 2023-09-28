f6573aef46f33e3861203eb409610821ec6db6d2
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2023-09-28 07:27:14 -0400

    Refactor healthcheck implementation (#22)

---
+++ src/clj/venn/agent/http/handlers/internal.clj
@@ -0,0 +1,11 @@
+(ns venn.agent.http.handlers.internal
+  (:require [ring.util.http-response :as http]
+            [xtdb.api :as xt]))
+
+
+(def health-response-schema [:map
+                             [:db map?]])
+
+
+(defn health [{:keys [db]}]
+  (http/ok {:db (xt/status db)}))
