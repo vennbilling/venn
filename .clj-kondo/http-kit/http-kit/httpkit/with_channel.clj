6a01483ed5585600d40900c421e4d22c687159fd
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2024-07-30 21:28:06 -0400

    formatting

--- .clj-kondo/http-kit/http-kit/httpkit/with_channel.clj
+++ .clj-kondo/http-kit/http-kit/httpkit/with_channel.clj
@@ -1,10 +1,7 @@
 (ns httpkit.with-channel
-  (:require
-    [clj-kondo.hooks-api :as api]))
+  (:require [clj-kondo.hooks-api :as api]))
 
-
-(defn with-channel
-  [{node :node}]
+(defn with-channel [{node :node}]
   (let [[request channel & body] (rest (:children node))]
     (when-not (and request     channel) (throw (ex-info "No request or channel provided" {})))
     (when-not (api/token-node? channel) (throw (ex-info "Missing channel argument" {})))
