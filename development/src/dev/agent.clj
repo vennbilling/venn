069c9a4baf5ece9765ad37dde60d319447f67b79
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2024-08-06 22:10:56 -0400

    Linter

--- development/src/dev/agent.clj
+++ development/src/dev/agent.clj
@@ -6,7 +6,8 @@
     [com.vennbilling.agent.core]
     [integrant.core :as ig]
     [integrant.repl :refer [prep go halt reset init]]
-    [integrant.repl.state]))
+    [integrant.repl.state]
+    [io.pedestal.log :as log]))
 
 
 (def config-file (io/resource "../../../bases/agent/resources/agent/system.edn"))
@@ -16,6 +17,9 @@
 (repl/set-refresh-dirs "../../../bases/agent/src")
 
 (System/setProperty "logback.statusListenerClass" "ch.qos.logback.core.status.OnConsoleStatusListener")
+
+
+(log/debug :msg "agent running in development mode")
 
 
 ;; Helpers to start and stop the agent
