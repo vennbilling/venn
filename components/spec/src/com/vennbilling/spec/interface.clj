8507562b49a4579c3cdeda1c80ceacc8fa3019de
Bryan Mikaelian <bryan@iceblock.tech>
2024-08-08 22:03:34 -0400

    Clean up some things around Identify (#32)

--- components/spec/src/com/vennbilling/spec/interface.clj
+++ components/spec/src/com/vennbilling/spec/interface.clj
@@ -1,14 +1,10 @@
 (ns com.vennbilling.spec.interface
   (:require
-    [com.vennbilling.spec.core :as core]))
+    [com.vennbilling.spec.identify :as identify]))
 
 
 ;; IDENTIFY
-
-;; TODO: This should be private but the customer API is using this
-(def identify-response-schema core/identify-response-schema)
-
-(def identify-route core/identify-route)
+(def identify-route identify/route)
 
 
 ;; CHARGE
