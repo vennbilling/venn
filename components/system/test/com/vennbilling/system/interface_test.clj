8a8115b0657063a17814bf831fbc474cd807ae25
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2024-08-09 13:00:29 -0400

    Remove tests since they aren't needed

--- components/system/test/com/vennbilling/system/interface_test.clj
+++
@@ -1,6 +0,0 @@
-(ns com.vennbilling.system.interface-test
-  (:require [clojure.test :as test :refer :all]
-            [com.vennbilling.system.interface :as system]))
-
-(deftest dummy-test
-  (is (= 1 1)))
