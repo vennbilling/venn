b891dec8e2f13e117fe4117cc43a443f9e586441
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2024-08-29 20:59:29 -0400

    Move all http-related config to system component

--- bases/agent/src/com/vennbilling/agent/core.clj
+++ bases/agent/src/com/vennbilling/agent/core.clj
@@ -1,101 +1,30 @@
 (ns com.vennbilling.agent.core
   (:gen-class)
   (:require
-    [aero.core :as aero]
     [clojure.java.io :as io]
     [com.vennbilling.customer.interface :as customer]
     [com.vennbilling.healthcheck.interface :as healthcheck]
     [com.vennbilling.spec.interface :as venn-spec]
     [com.vennbilling.system.interface :as system]
-    [integrant.core :as ig]
-    [io.pedestal.log :as log]
-    [muuntaja.core :as m]
-    [reitit.coercion.malli :as malli]
-    [reitit.ring :as ring]
-    [reitit.ring.coercion :as coercion]
-    [reitit.ring.middleware.exception :as exception]
-    [reitit.ring.middleware.muuntaja :as muuntaja]
-    [reitit.ring.middleware.parameters :as parameters]
-    [ring.adapter.undertow :refer [run-undertow]]
-    [ring.logger :as logger]))
-
-
-(defn internal-routes
-  [_opts]
-  [healthcheck/simple-route])
+    [io.pedestal.log :as log]))
 
 
 (def api-routes
-  [venn-spec/identify-route
-   customer/list-route
-   customer/show-route])
+  ["/v1"
+   [venn-spec/identify-route
+    customer/list-route
+    customer/show-route
 
-
-(def router-config
-  {:coercion malli/coercion
-   :muuntaja m/instance
-   :middleware [;; query-params & form-params
-                parameters/parameters-middleware
-                ;; content-negotiation
-                muuntaja/format-negotiate-middleware
-                ;; encoding response body
-                muuntaja/format-response-middleware
-                ;; exception handling
-                exception/exception-middleware
-                ;; decoding request body
-                muuntaja/format-request-middleware
-                ;; coercing response bodys
-                coercion/coerce-response-middleware
-                ;; coercing request parameters
-                coercion/coerce-request-middleware]})
-
-
-;; TODO Move this to system component
-(defmethod ig/init-key :http/server
-  [_ opts]
-  (let [handler (atom (delay (:handler opts)))
-        undertow-opts (dissoc opts :handler)]
-    {:server (run-undertow (fn [req] (@@handler req)) undertow-opts)}))
-
-
-;; TODO Move this to system component
-(defmethod ig/halt-key! :http/server
-  [_ {:keys [server]}]
-  (.stop server))
-
-
-;; TODO Move this to system component
-(defmethod ig/init-key :handler/ring
-  [_ {:keys [router]}]
-  (logger/wrap-with-logger
-    (ring/ring-handler
-      router
-      (ring/create-default-handler))))
-
-
-;; TODO Move this to system component
-(defmethod ig/init-key :router/ring
-  [_ {:keys [base-path]}]
-  (ring/router [base-path router-config api-routes]))
+    healthcheck/simple-route]])
 
 
 (def ^:const banner (slurp (io/resource "agent/banner.txt")))
 (def config-file (io/resource "agent/system.edn"))
 
 
-;; TODO Move this to system component
-(def settings
-  {:http/server
-   {:handler (ig/ref :handler/ring)}
-
-   :handler/ring
-   {:router (ig/ref :router/ring)}})
-
-
 (defn -main
   [& _]
   ;; TODO: Pass profile as args
-  (let [config (aero/read-config config-file {:profile :prod})
-        agent (merge-with into config settings)]
+  (let [agent (system/create-http config-file :prod api-routes)]
     (system/start banner agent))
   (log/info :msg "venn agent started successfully."))
---
+++ components/system/src/com/vennbilling/system/core.clj
@@ -0,0 +1,41 @@
+(ns com.vennbilling.system.core
+  (:require
+    [aero.core :as aero]
+    [clojure.java.io :as io]
+    [com.vennbilling.system.http :as http]
+    [io.pedestal.log :as log])
+  (:import
+    (java.io
+      FileNotFoundException)))
+
+
+(def ^:private default-config (io/resource "system/default.edn"))
+
+
+(defn- load-defaults
+  []
+  (aero/read-config default-config {:profile :dev}))
+
+
+(defn- init
+  [config-file profile]
+  (try
+    (let [config (aero/read-config config-file {:profile profile})]
+      (merge config {}))
+    (catch FileNotFoundException _
+      ;; TODO: Log Exception
+      (log/warn :msg "invalid aero config. using default.")
+      (load-defaults))
+    (catch IllegalArgumentException _
+      ;; TODO: Log Exception
+      (log/warn :msg "invalid aero config. using default.")
+      (load-defaults))))
+
+
+(defn create-http
+  [config-file profile routes]
+
+  (let [config (init config-file profile)
+        settings (http/server-settings routes)
+        system (merge-with into config settings)]
+    system))
