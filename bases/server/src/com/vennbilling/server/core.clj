88990f6d8b6155cf3087af6b8c78ca1d68225ce6
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2024-08-30 20:37:45 -0400

    New server base + project

--- bases/server/src/com/vennbilling/server/core.clj
+++ bases/server/src/com/vennbilling/server/core.clj
@@ -1,26 +1,27 @@
-(ns com.vennbilling.agent.core
+(ns com.vennbilling.server.core
   (:gen-class)
   (:require
     [clojure.java.io :as io]
+    [com.vennbilling.customer.interface :as customer]
     [com.vennbilling.healthcheck.interface :as healthcheck]
-    [com.vennbilling.spec.interface :as venn-spec]
     [com.vennbilling.system.interface :as system]
     [io.pedestal.log :as log]))
 
 
 (def api-routes
   ["/v1"
-   [venn-spec/identify-route
+   [customer/list-route
+    customer/show-route
+
     healthcheck/simple-route]])
 
 
-(def ^:const banner (slurp (io/resource "agent/banner.txt")))
-(def config-file (io/resource "agent/system.edn"))
+(def ^:const banner (slurp (io/resource "server/banner.txt")))
+(def config-file (io/resource "server/system.edn"))
 
 
 (defn -main
   [& _]
-  ;; TODO: Pass profile as args
   (let [agent (system/create-http config-file :prod api-routes)]
     (system/start banner agent))
-  (log/info :msg "venn agent started successfully."))
+  (log/info :msg "venn server started successfully."))
---
+++ bases/server/test/com/vennbilling/server/core_test.clj
@@ -0,0 +1,6 @@
+(ns com.vennbilling.server.core-test
+  (:require [clojure.test :as test :refer :all]
+            [com.vennbilling.server.core :as core]))
+
+(deftest dummy-test
+  (is (= 1 1)))
