ed7a9f33be851e2fedd93c7c1359f2d19f68d7bc
Bryan Mikaelian <bryan@iceblock.tech>
2024-08-30 20:48:45 -0400

    Create Venn Server Project + Base (#37)
    * Restructure dev project to just be a monolith service
    
    * New server base + project
    
    * CI updates for new project

---
+++ development/src/dev/venn.clj
@@ -0,0 +1,49 @@
+(ns dev.venn
+  (:require
+    [clojure.java.io :as io]
+    [clojure.tools.namespace.repl :as repl]
+    [com.vennbilling.customer.interface :as customer]
+    [com.vennbilling.healthcheck.interface :as healthcheck]
+    [com.vennbilling.spec.interface :as venn-spec]
+    [com.vennbilling.system.interface :as system]
+    [integrant.core :as ig]
+    [integrant.repl :refer [prep go halt reset init]]
+    [integrant.repl.state]))
+
+
+(def config-file (io/resource "system.edn"))
+(def profile :dev)
+(def base-path "/v1")
+
+
+(def agent-routes
+  [venn-spec/identify-route
+   healthcheck/simple-route])
+
+
+(def server-routes
+  [customer/list-route
+   customer/show-route])
+
+
+(def all-routes (apply conj agent-routes server-routes))
+
+
+;; In development, Venn is a single service with all the routes
+(def routes (conj [base-path] all-routes))
+
+(def venn (system/create-http config-file profile routes))
+
+(integrant.repl/set-prep! #(ig/prep venn))
+(repl/set-refresh-dirs "../../../components")
+
+
+;; Helpers to start and stop the monolith
+(comment
+  (prep)
+  (init)
+  integrant.repl.state/config
+  integrant.repl.state/system
+  (go)
+  (halt)
+  (reset))
