aca1eecf6c14ffd413ec90fe9cb79a7692eae13d
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2024-11-09 10:04:36 -0500

    Initial DB component

---
+++ components/database/test/com/vennbilling/database/interface_test.clj
@@ -0,0 +1,6 @@
+(ns com.vennbilling.database.interface-test
+  (:require [clojure.test :as test :refer :all]
+            [com.vennbilling.database.interface :as database]))
+
+(deftest dummy-test
+  (is (= 1 1)))
