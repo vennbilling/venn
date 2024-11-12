c625d3efd2e96d582f186a757d68f5c437a6421a
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2024-11-12 15:23:02 -0500

    Fix issue with name in server base

--- bases/server/src/com/vennbilling/server/core.clj
+++ bases/server/src/com/vennbilling/server/core.clj
@@ -1,12 +1,11 @@
 (ns com.vennbilling.server.core
   (:gen-class)
   (:require
-    [clojure.java.io :as io]
-    [com.vennbilling.customer.interface :as customer]
-    [com.vennbilling.healthcheck.interface :as healthcheck]
-    [com.vennbilling.logging.interface :as log]
-    [com.vennbilling.system.interface :as system]))
-
+   [clojure.java.io :as io]
+   [com.vennbilling.customer.interface :as customer]
+   [com.vennbilling.healthcheck.interface :as healthcheck]
+   [com.vennbilling.logging.interface :as log]
+   [com.vennbilling.system.interface :as system]))
 
 (def api-routes
   ["/v1"
@@ -15,13 +14,11 @@
 
     healthcheck/simple-route]])
 
-
 (def ^:const banner (slurp (io/resource "server/banner.txt")))
 (def config-file (io/resource "server/system.edn"))
 
-
 (defn -main
   [& _]
-  (let [agent (system/create-http config-file :prod api-routes)]
-    (system/start banner agent))
+  (let [server (system/init config-file :prod api-routes)]
+    (system/start banner server))
   (log/info "venn server started successfully."))
