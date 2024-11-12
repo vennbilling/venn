980a9c9453631bbfb9c971351eeaaba83b9006ea
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2024-11-12 15:22:35 -0500

    Switch to aero profiles

--- bases/agent/src/com/vennbilling/agent/core.clj
+++ bases/agent/src/com/vennbilling/agent/core.clj
@@ -1,26 +1,23 @@
 (ns com.vennbilling.agent.core
   (:gen-class)
   (:require
-    [clojure.java.io :as io]
-    [com.vennbilling.healthcheck.interface :as healthcheck]
-    [com.vennbilling.logging.interface :as log]
-    [com.vennbilling.spec.interface :as venn-spec]
-    [com.vennbilling.system.interface :as system]))
-
+   [clojure.java.io :as io]
+   [com.vennbilling.healthcheck.interface :as healthcheck]
+   [com.vennbilling.logging.interface :as log]
+   [com.vennbilling.spec.interface :as venn-spec]
+   [com.vennbilling.system.interface :as system]))
 
 (def api-routes
   ["/v1"
    [venn-spec/identify-route
     healthcheck/simple-route]])
 
-
 (def ^:const banner (slurp (io/resource "agent/banner.txt")))
 (def config-file (io/resource "agent/system.edn"))
 
-
 (defn -main
   [& _]
-  ;; TODO: Pass profile as args
+  ;; TODO: Pass profile as args or env var
   (let [agent (system/init config-file :prod api-routes)]
     (system/start banner agent))
   (log/info "venn agent started successfully."))
