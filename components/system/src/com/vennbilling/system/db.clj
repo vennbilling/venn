906747c8e11d7d67a227d5ce41a74c77387ecf21
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2024-11-19 08:19:04 -0500

    Cleanup

--- components/system/src/com/vennbilling/system/db.clj
+++ components/system/src/com/vennbilling/system/db.clj
@@ -1,14 +1,15 @@
 (ns com.vennbilling.system.db
   (:require [integrant.core :as ig]
-            [next.jdbc :as jdbc]
-            [next.jdbc.connection :refer [jdbc-url]])
+            [next.jdbc.connection :refer [jdbc-url]]
+
+            [com.vennbilling.logging.interface :as l])
 
   (:import (com.zaxxer.hikari HikariConfig
                               HikariDataSource)))
 
 (defn- init-connection-pool-config
   [url]
-  (let [config (new HikariConfig)]
+  (let [^HikariConfig config (new HikariConfig)]
     (-> config
         (.setJdbcUrl url))
     config))
@@ -18,13 +19,23 @@
   (let [{:keys [managed-connection?]} db
         migration-dir-config (dissoc db :managed-connection?)
         url (jdbc-url db)
-        config (init-connection-pool-config url)
-        ds (new HikariDataSource config)
-        conn (jdbc/get-connection ds)]
-    (into {:db {:datasource (.getConnection ds)
-                :connection conn
-                :managed-connection? managed-connection?}}
-          migration-dir-config)))
+        config (init-connection-pool-config url)]
+
+    (try
+      (let [^HikariDataSource ds (new HikariDataSource config)
+            test-conn (.getConnection ds)]
+        (l/info "Database connection healthy")
+        (.close test-conn)
+
+        ;; TODO: This feels weird. We are forming a config in a structure migratus.core wants. 
+        ;; Ideally, :db/server shouldn't understand or care. A separate key, :db/migrations should handle this
+        (into {:db {:datasource ds
+                    :managed-connection? managed-connection?}}
+              migration-dir-config))
+      (catch Exception e
+        (l/warn (str "Failed to connect to database. err: " e))
+        (throw e)))))
+
 (defmethod ig/halt-key! :db/server [_ {:keys [db]}]
   (let [{:keys [datasource]} db]
     (.close datasource)))
