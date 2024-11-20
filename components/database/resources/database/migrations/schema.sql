7550912527c95f4d9a43fedde3aaba0335736a28
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2024-11-20 08:06:33 -0500

    Have docker compose manage creation of databases

--- components/database/resources/database/migrations/schema.sql
+++ components/database/resources/database/migrations/schema.sql
@@ -1,6 +1,1 @@
-SELECT 'CREATE DATABASE venn_development'
-WHERE NOT EXISTS (SELECT FROM pg_databases WHERE datname = 'venn_development');
 
-
-SELECT 'CREATE DATABASE venn_test'
-WHERE NOT EXISTS (SELECT FROM pg_databases WHERE datname = 'venn_test');
